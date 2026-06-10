2dd5a5d0ee664c0da13880a684d9ede029dea210
Lucia Fresno <luciafresno@gmail.com>
2026-06-10 20:36:54 -0300

    Termino de armar el RDS de edges completo. Posible mejora: agregar edad. Para evaluar a futuro. #11

--- dataset-r34-enerosydiciembres-rem-real.r
+++ dataset-r34-enerosydiciembres-rem-real.r
@@ -3,10 +3,8 @@ library(tidyr)
 library(lubridate)
 df_original <- readRDS("./materiales/df_mujer_real.rds")
 
-
-
 df_original <- df_original %>%
-    select(id_trabajador, tiempo, r34) %>%
+    select(id_trabajador, tiempo, r34, edad) %>%
     group_by(id_trabajador, r34) %>%
     arrange(desc(id_trabajador), tiempo) %>%
     ungroup() %>%
@@ -24,7 +22,7 @@ identifico_intervalos <- function(df) {
     fin_index <- inicio_index + 1
 
     inicio_df  <- df %>%
-        select(id_trabajador, tiempo, intervalo) %>%
+        select(id_trabajador, tiempo, intervalo) %>%       # , fnacim
         slice(inicio_index) %>%
         rename(inicio = tiempo)
 
@@ -40,18 +38,24 @@ identifico_intervalos <- function(df) {
 inicio_fin <- identifico_intervalos(df_original)
 
 calculo_eneros <- function(inicio, intervalo, id_trabajador, fin) {
+    # , fnacim
     start_time <- Sys.time()
-    enero = update(    inicio, year = year(inicio) + 1, month = 1, day = 1 )
+    enero = update(inicio,
+                   year = year(inicio) + 1,
+                   month = 1,
+                   day = 1)
     a <- tibble(
         id_trabajador = numeric(),
-        enero = Date(),
+        tiempo = Date(),
         r34 = numeric()
+        # posible mejora: edad
     )
-    while (fin>enero) {
+    while (fin > enero) {
         a <- a %>% add_row (
             id_trabajador = id_trabajador,
-            enero = enero,
+            tiempo = enero,
             r34 = 999
+            # posible mejora: edad
         )
 
 
@@ -59,38 +63,76 @@ calculo_eneros <- function(inicio, intervalo, id_trabajador, fin) {
     }
     return(a)
 }
+calculo_diciembres <- function(inicio, intervalo, id_trabajador, fin) {
+    # , fnacim
+    start_time <- Sys.time()
+    diciembre = update(inicio,
+                       year = year(inicio) + 1,
+                       month = 12,
+                       day = 1)
+    a <- tibble(
+        id_trabajador = numeric(),
+        tiempo = Date(),
+        r34 = numeric()
+        # posible mejora: edad
+    )
+    while (fin > diciembre) {
+        a <- a %>% add_row (
+            id_trabajador = id_trabajador,
+            tiempo = diciembre,
+            r34 = 999
+            # posible mejora: edad
+        )
+
+
+        diciembre = ymd(diciembre) %m+% years(1)
+    }
+    return(a)
+}
 
 # seleccion1 <- inicio_fin %>% slice(1:50000)
-# eneros1 <- pmap_dfr(seleccion1, calculo_eneros)
 # seleccion2 <- inicio_fin %>% slice(50001:100000)
-# eneros2 <- pmap_dfr(seleccion2, calculo_eneros)
 # seleccion3 <- inicio_fin %>% slice(100001:150000)
-# eneros3 <- pmap_dfr(seleccion3, calculo_eneros)
 # seleccion4 <- inicio_fin %>% slice(150001:200000)
-# eneros4 <- pmap_dfr(seleccion4, calculo_eneros)
 # seleccion5 <- inicio_fin %>% slice(200001:250000)
-# eneros5 <- pmap_dfr(seleccion5, calculo_eneros)
 # seleccion6 <- inicio_fin %>% slice(250001:300000)
-# eneros6 <- pmap_dfr(seleccion6, calculo_eneros)
 # seleccion7 <- inicio_fin %>% slice(300001:311276)
-# eneros7 <- pmap_dfr(seleccion7, calculo_eneros)
+
+
+# eneros1 <- map_dfr(seleccion1, calculo_eneros) ...
+# diciembress1 <- map_dfr(seleccion1, calculo_diciembres) ...
 
 library(data.table)
-eneros_list <- list(eneros1, eneros2, eneros3, eneros4, eneros5, eneros6, eneros7)
-nuevos_nodos <- rbindlist(eneros_list, fill = TRUE)
-diciembres_list <- list()
+eneros_list <- list(
+    eneros1,
+    eneros2,
+    eneros3,
+    eneros4,
+    eneros5,
+    eneros6,
+    eneros7
+)
 
-
-df_con_nuevos_nodos <- bind_rows(df_original %>% select(-intervalo)
-                                 , nuevos_nodos) %>% arrange(id_trabajador, tiempo)
-
-df_solo_eneros_y_diciembres <- df_con_nuevos_nodos %>% filter(month(tiempo) == 1 |
-                                                                  month(tiempo) == 12) %>% arrange(id_trabajador, tiempo)
-
-df_sig_r34 <- df_solo_eneros_y_diciembres %>%
+diciembres_list <- list(
+    diciembres1,
+    diciembres2,
+    diciembres3,
+    diciembres4,
+    diciembres5,
+    diciembres6,
+    diciembres7
+)
+nuevos_edges <- rbindlist(append(eneros_list, diciembres_list),
+                          fill = TRUE,
+                          use.names = TRUE)
+df_completo_eneros_diciembres <- bind_rows(df_original %>%
+                                               select(-intervalo) %>%
+                                               filter(month(tiempo) == 1 | month(tiempo) == 12)
+                                           , nuevos_edges) %>%
+    arrange(id_trabajador, tiempo) %>%
     mutate(
         sig_r34 = ifelse(id_trabajador == lead(id_trabajador, default = 999), lead(r34), 0),
         # sig_r34 es a donde transiciona en el sig registro: 999 es cuando se va un ratito, sólo es 0 cuando es el último registro de ese id_trabajador (sale del sistema)
-
     )
-saveRDS(df_sig_r34, file = "./materiales/edges.rds")
+
+saveRDS(df_completo_eneros_diciembres, file = "./materiales/edges.rds")
